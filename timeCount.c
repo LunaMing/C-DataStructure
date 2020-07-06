1a7eae7dac3bf42d15738115e4486126ea434907
Yueming Liu <44162157+LunaMing@users.noreply.github.com>
2020-07-06 14:42:11 +0800

    随便写写测试代码，试验一下计时功能

--- timeCount.c
+++ timeCount.c
@@ -6,15 +6,25 @@ clock_t start, stop;
 double duration;
 /* 记录被测函数运行时间，以秒为单位 */
 int main() {
+	int N = 10000;
 	/* 不在测试范围内的准备工作写在clock()调用之前*/
 	start = clock();	/* 开始计时 */
-	MyFunction();		/* 把被测函数加在这里 */
+	MyFunction(N);		/* 把被测函数加在这里 */
 	stop = clock();		/* 停止计时 */
 	duration = ((double)(stop - start)) / CLK_TCK;
 	/* 计算运行时间 */
 	/* 其他不在测试范围的处理写在后面，例如输出duration的值 */
+	printf("%d", duration);
 	return 0;
 }
-void MyFunction() {
-
+void MyFunction(int N) {
+	double j = 0;
+	//随便写写
+	for (int i = 0; i < N; i++)
+	{
+		for (int k = 0; k < N; k++)
+		{
+			j++;
+		}
+	}
 }
