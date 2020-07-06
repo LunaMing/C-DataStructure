548472fd856b43b65d71f0c3c8d0311785b3cd36
Yueming Liu <44162157+LunaMing@users.noreply.github.com>
2020-07-06 15:09:19 +0800

    按照老师的写代码

--- timeCount.c
+++ timeCount.c
@@ -1,30 +1,59 @@
 ﻿#include<stdio.h>
+#include<math.h>
 #include<time.h>
-void MyFunction();
 clock_t start, stop;
-/* clock_t是clock()函数返回的变量类型 */
 double duration;
-/* 记录被测函数运行时间，以秒为单位 */
+#define MAXN 100	/*多项式最大项数，即多项式阶数+1*/
+double f1(int n, double a[], double x);
+double f2(int n, double a[], double x);
+#define MAXK 1		/*试验次数*/
+
 int main() {
-	int N = 10000;
-	/* 不在测试范围内的准备工作写在clock()调用之前*/
+	double a[MAXN];	/*存储多项式的系数*/
+	for (int i = 0; i < MAXN; i++)
+	{
+		a[i] = i;
+	}
+
 	start = clock();	/* 开始计时 */
-	MyFunction(N);		/* 把被测函数加在这里 */
+	for (int i = 0; i < MAXK; i++)
+	{
+		f1(MAXN - 1, a, 1.1);
+	}
 	stop = clock();		/* 停止计时 */
 	duration = ((double)(stop - start)) / CLK_TCK;
-	/* 计算运行时间 */
-	/* 其他不在测试范围的处理写在后面，例如输出duration的值 */
-	printf("%d", duration);
+	printf("ticks1 = %f\n", (double)(stop - start));
+	printf("duration1 = %6.2e\n", duration);
+
+	start = clock();	/* 开始计时 */
+	for (int i = 0; i < MAXK; i++)
+	{
+		f2(MAXN - 1, a, 1.1);
+	}
+	stop = clock();		/* 停止计时 */
+	duration = ((double)(stop - start)) / CLK_TCK;
+	printf("ticks1 = %f\n", (double)(stop - start));
+	printf("duration1 = %6.2e\n", duration);
+
 	return 0;
 }
-void MyFunction(int N) {
-	double j = 0;
-	//随便写写
-	for (int i = 0; i < N; i++)
+
+double f1(int n, double a[], double x) {
+	//正常写，按照式子
+	double p = a[0];
+	for (int i = 1; i <= n; i++)
 	{
-		for (int k = 0; k < N; k++)
-		{
-			j++;
-		}
+		p += (a[i] * pow(x, i));
 	}
+	return p;
+}
+
+double f2(int n, double a[], double x) {
+	//用秦九韶
+	double p = a[n];
+	for (int i = n; i > 0; i--)
+	{
+		p = a[i - 1] + x * p;
+	}
+	return p;
 }
