c0efbe5ba86a8590608d923de0ec977d6ef18ae5
Yueming Liu <44162157+LunaMing@users.noreply.github.com>
2020-07-12 14:30:05 +0800

    调整注释位置

--- MaxSubseqSum.cpp
+++ MaxSubseqSum.cpp
@@ -13,13 +13,15 @@ int MaxSubseqSum1(vector<int> A) {
     for (i = 0; i < N; i++) {
 		/* i是子列左端位置*/
         for (j = i; j < N; j++) {
-            /* j是子列右端位置*/
+        /* j是子列右端位置*/
             ThisSum = 0;
             /* ThisSum是从A[i]到A[j]的子列和*/
             for (k = i; k <= j; k++)
                 ThisSum += A[k];
-            if (ThisSum > MaxSum) /* 如果刚得到的这个子列和更大*/
-                MaxSum = ThisSum;    /* 则更新结果*/
+            if (ThisSum > MaxSum) 
+            /* 如果刚得到的这个子列和更大*/
+                MaxSum = ThisSum;    
+                /* 则更新结果*/
         } /* j循环结束*/
     } /* i循环结束*/
     return MaxSum;
